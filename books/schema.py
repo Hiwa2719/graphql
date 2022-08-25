617dfcd96682f28f9646fcdbb84926cf4c5e1971
hiwa2719 <hiahmadyan@gmail.com>
2022-08-25 16:52:08 +0430

    adding books query

---
+++ books/schema.py
@@ -0,0 +1,21 @@
+import graphene
+
+from graphene_django import DjangoObjectType
+
+from .models import Book
+
+
+class BooksType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class Query(graphene.ObjectType):
+    all_books = graphene.List(BooksType)
+
+    def resolve_all_books(self, info):
+        return Book.objects.all()
+
+
+schema = graphene.Schema(query=Query)
