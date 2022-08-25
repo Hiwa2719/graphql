c78a43cb0653259158ba4e653f63cbb4556c0634
hiwa2719 <hiahmadyan@gmail.com>
2022-08-25 21:41:02 +0430

    adding Category Update, Create, Delete mutations

--- quiz/schema.py
+++ quiz/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from django.shortcuts import get_object_or_404
 from graphene_django import DjangoObjectType, DjangoListField
 from .models import Quiz, Answer, Category, Question
 
@@ -32,4 +33,47 @@ class Query(graphene.ObjectType):
     all_questions = DjangoListField(QuestionType)
 
 
-schema = graphene.Schema(query=Query)
+class CategoryUpdate(graphene.Mutation):
+    category = graphene.Field(CategoryType)
+
+    class Arguments:
+        category_id = graphene.Int(required=True)
+        name = graphene.String(required=True)
+
+    def mutate(self, info, category_id, name):
+        category = get_object_or_404(Category, id=category_id)
+        category.name = name
+        category.save()
+        return CategoryUpdate(category=category)
+
+
+class CreateCategory(graphene.Mutation):
+    category = graphene.Field(CategoryType)
+
+    class Arguments:
+        name = graphene.String(required=True)
+
+    def mutate(self, info, name):
+        category = Category.objects.create(name=name)
+        return CreateCategory(category=category)
+
+
+class DeleteCategory(graphene.Mutation):
+    category = graphene.Int()
+
+    class Arguments:
+        category_id = graphene.Int(required=True)
+
+
+    def mutate(self, info, category_id):
+        category = get_object_or_404(Category, id=category_id).delete()
+        return CreateCategory(category=category_id)
+
+
+class Mutation(graphene.Mutation):
+    update_category = CategoryUpdate.Field()
+    create_category = CreateCategory.Field()
+    delete_category = DeleteCategory.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
